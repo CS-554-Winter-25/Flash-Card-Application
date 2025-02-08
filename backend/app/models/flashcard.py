97c03d392bc911d93d37581e05c554cbc3114d2e
felip3s <felip3@pdx.edu>
2025-02-08 12:26:50 -0800

    Sketched a sqlite database model, insert and retrive functions

--- backend/app/models/flashcard.py
+++ backend/app/models/flashcard.py
@@ -2,7 +2,10 @@
 Flashcard.py
 """
 
-class flashcard:
+import model_sqlite3
+
+
+class Flashcard:
     def __init__(self, question: str, answer: str):
         self.data = {"question": question, "answer": answer}
 
@@ -24,11 +27,23 @@ class flashcard:
         '''
         return self.data
     
- 
-test_card1 = flashcard("What is Flask?", "A lightweight Python web framework.")
-test_card2 = flashcard("What is React?", "A JavaScript library for building UIs.")
 
-flashcards = [test_card1.get_data(), test_card2.get_data()]
+# Test data
+
+test_card1 = Flashcard("What is Flask?", "A lightweight Python web framework.")
+test_card2 = Flashcard("What is React?", "A JavaScript library for building UIs.")
+
+deck_db = Model_sqlite()
+deck_db.insert(test_card1)
+deck_db.insert(test_card2)
+
+# flashcards = [test_card1.get_data(), test_card2.get_data()]
 
 def get_all_flashcards():
+    flashcards = []
+    model = Model_sqlite()
+    rows = model.select()
+    for row in rows:
+        card = Flashcard(row[0], row[1])
+        flashcards.append(Flashcard.get_data())
     return flashcards   
