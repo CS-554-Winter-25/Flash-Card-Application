fb46dde77ea9cfe536023cb90c17f5e87438449c
felip3s <felip3@pdx.edu>
2025-02-07 17:29:09 -0800

    created flashcard class: constructor, display, get_question/answer

--- backend/app/models/flashcard.py
+++ backend/app/models/flashcard.py
@@ -2,10 +2,32 @@
 Flashcard.py
 """
 
-flashcards = [
-    {"question": "What is Flask?", "answer": "A lightweight Python web framework."},
-    {"question": "What is React?", "answer": "A JavaScript library for building UIs."}
-]
+class flashcard:
+    def __init__(self, question: str, answer: str):
+        self.data = {"question": question, "answer": answer}
+
+    def get_question(self):
+        '''
+        Returns the question of a flashcard
+        '''
+        return self.data["question"]
+    
+    def get_answer(self):
+        '''
+        Returns the answer of a flashcard
+        '''
+        return self.data["answer"]
+        
+    def __str__(self):
+        '''
+        Displayes the entire flashcard
+        '''
+        return f"Question: {self.get_question}\nAnswer: {self.get_answer}"
+    
+ 
+test_card1 = flashcard("What is Flask?", "A lightweight Python web framework.")
+test_card2 = flashcard("What is React?", "A JavaScript library for building UIs.")
+flashcards = [test_card1, test_card2]
 
 def get_all_flashcards():
-    return flashcards
+    return flashcards   
