6ac088c6ee165678f510fa54c5af0bf4479ddb12
felip3s <felip3@pdx.edu>
2025-02-21 16:10:33 -0800

    reverted backend changes

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,11 +1,8 @@
-from flask import Flask, jsonify, session, redirect
+from flask import Flask, jsonify
 from flask_cors import CORS
 from src.models import db
 from src.routes import api
 from sqlalchemy import event
-from flask_oauthlib.client import OAuth
-from oauth_config import client_id, client_secret, authorization_base_url, token_url
-
 
 app = Flask(__name__)
 app.config["SESSION_SQLALCHEMY"] = db
@@ -13,19 +10,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db.init_app(app)
 api.init_app(app)
 CORS(app)
-
-
-oauth = OAuth(app)
-
-google = oauth.register(
-    name = 'google',
-    client_id = client_id,
-    client_secret = client_secret,
-    request_token_params={'scope': 'email'},
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url=token_url,
-    authorize_url=authorization_base_url)
 
 with app.app_context():
     @event.listens_for(db.engine, "connect")
@@ -38,37 +22,8 @@ with app.app_context():
 
 @app.route('/message', methods=['GET'])
 def hello():
-    if 'token' in session:
-        return jsonify({"message": "Flashcards Main Menu"})
-    return jsonify({"message": "Please login to access the Flashcards app"})
+    return jsonify({"message": "Flashcards Main Menu"})
 
-@app.route('/login/google')
-def login_google():
-    try:
-        redirect_uri = url_for('authorize_google', _external=True)
-        return google.authorize_redirect(redirect_uri)
-    except Exception as e:
-        return jsonify({"error": str(e)}, 500)
-
-@app.route('/logout')
-def logout():
-    session.pop('token', None)
-    return redirect(url_for('hello'))
-
-@app.route('/authorize/google')
-def authorize_google():
-    token = google.authorize_access_token()
-    userinfo = google.server_metadata.get('userinfo_endpoint')
-    resp = google.get('userinfo')
-    user_info = resp.json()
-    username = user_info['email']
-
-    session['username'] = username
-    session['oauth_token'] = token
-
-    if resp is None or token is None:
-        return jsonify({"Access denied": resp.get('error')}, resp.get('error_description'))
-    return redirect(url_for('hello'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- oauth_config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-#Will need to get these out of the codebase!!!
-client_id = os.environ.get('115245670253-l0hn80magke9s5orhrs7c8m5q4run172.apps.googleusercontent.com')
-client_secret = os.environ.get('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')
-redirect_callback = os.environ.get('http://localhost:5173/authorize/google')
-authorization_base_url = 'https://accounts.google.com/o/oauth2/auth'
-token_url = 'https://accounts.google.com/o/oauth2/token'
