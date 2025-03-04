42b76663b392b6dda7823df483ef683a6df919d2
felip3s <felip3@pdx.edu>
2025-03-04 15:10:23 -0800

    Added edit and delete buttons to card.

--- frontend/src/pages/EditFlashcard.jsx
+++ frontend/src/pages/EditFlashcard.jsx
@@ -1,28 +1,39 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { handleUpdateFlashcard, handleDeleteFlashcard } from '../components/ApiCall.jsx';
+import { useAppContext } from "../AppContext";
 
-function EditFlashcard() {
+function EditFlashcard( {card} ) {
+    const navigate = useNavigate();
+    const [editedFlashcard, setEditedFlashcard] = useState({ question: card.question, answer: card.answer, topic_id: card.topic_id });
+    const [topicName, setTopicName] = useState('');
+    const [topicIdInput, setTopicIdInput] = useState(card.topic_id);
+    const { topics } = useAppContext
     return (
         <div>
             <h2>Edit Flashcard</h2>
                 <input
             type="text"
             placeholder="Enter flashcard question"
-            value={newFlashcard.question}
-            onChange={(e) => setNewFlashcard({ ...newFlashcard, question: e.target.value })}
+            value={editedFlashcard.question}
+            onChange={(e) => setEditedFlashcard({ ...editedFlashcard, question: e.target.value })}
           />
           <input
             type="text"
             placeholder="Enter flashcard answer"
-            value={newFlashcard.answer}
-            onChange={(e) => setNewFlashcard({ ...newFlashcard, answer: e.target.value })}
+            value={editedFlashcard.answer}
+            onChange={(e) => setEditedFlashcard({ ...editedFlashcard, answer: e.target.value })}
           />
           <button
-            onClick={() => {
-              const updatedFlashcard = { ...newFlashcard };
-              const flashcardId = topicData.flashcards[editingIndex].id;
-
+            onClick={ async () => {
+              try {
+                await handleUpdateFlashcard(card.id, editedFlashcard, setEditedFlashcard);
+                } catch (error) {
+                  console.error('Error updating flashcard:', error);
+              }
               handleUpdateFlashcard(
                 flashcardId,
-                updatedFlashcard,
+                editedFlashcard,
                 setTopicData,
                 topicData,
                 editingIndex,
@@ -34,11 +45,16 @@ function EditFlashcard() {
             Save Changes
           </button>
           <button onClick={() => setCurrentPage('view-by-topic')}>Cancel</button>
-          <button onClick={() => {
-            const flashcardId = topicData.flashcards[editingIndex].id;
-            handleDeleteFlashcard(flashcardId, setTopicData);
-            setCurrentPage('view-by-topic')
+          {/*
+          <button onClick={ async () => {
+            try {
+              await handleDeleteFlashcard(card.id);
+              navigate('/');
+            } catch (error) {
+              console.error('Error deleting flashcard:', error);
+            }
           }}>Delete</button>
+          */}
         </div>
       )}
 
