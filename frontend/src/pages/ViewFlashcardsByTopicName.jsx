4cfed652d9b350f737e31c1fcdab34f14ac730dd
felip3s <felip3@pdx.edu>
2025-03-05 14:24:21 -0800

    Dropdown for topic name

--- frontend/src/pages/ViewFlashcardsByTopicName.jsx
+++ frontend/src/pages/ViewFlashcardsByTopicName.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import { AppProvider } from '../AppContext.jsx';
 import { handleFetchFlashcardsByTopicName } from '../components/ApiCall.jsx';
 import FlashcardList from '../components/FlashcardList/FlashcardList.jsx';
+import { useAppContext } from "../AppContext";
 
 function ViewFlashcardsByTopicName() {
   const [topicNameInput, setTopicNameInput] = useState('');
+  const [topicName, setTopicName] = useState('');
+  const { topics, setTopics } = useAppContext(); 
+  const [topicIdInput, setTopicIdInput] = useState('');
+    
 
   const [topicData, setTopicData] = useState({
     topicId: null,
@@ -12,21 +17,31 @@ function ViewFlashcardsByTopicName() {
     flashcards: []
   });
 
+  const handleTopicChange = (e) => {
+    setTopicName(e.target.value);
+    const selectedTopic = topics.find(topic => topic.topic === e.target.value);
+    if (selectedTopic) {
+      setTopicIdInput(selectedTopic.id);
+    }
+  };
+
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Enter topic name"
-        value={topicNameInput}
-        onChange={(e) => setTopicNameInput(e.target.value)}
-      />
+      <select value={topicName} onChange={handleTopicChange} className="input">
+          <option value="" disabled>Select topic</option>
+          {topics.map((topic) => (
+              <option key={topic.id} value={topic.topic}>
+              {topic.topic}
+              </option>
+          ))}
+      </select>
       <button
         className="fetch-button"
         onClick={
 
           async () => {
             try {
-              await handleFetchFlashcardsByTopicName(topicNameInput, setTopicData);
+              await handleFetchFlashcardsByTopicName(topicName, setTopicData);
             } catch (error) {
               console.error('Error fetching topic by name:', error);
             }
