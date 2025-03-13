e2b0277b8ae5aa921b9a3531d1e15e7b7e0dcb2b
Your Name <you@example.com>
2025-03-12 20:41:04 -0700

    typo

--- frontend/src/pages/Study.jsx
+++ frontend/src/pages/Study.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { fetchFlashcardsByTopic } from '../components/ApiCall';
 import Flashcard from '../components/Flashcard/Flashcard';
