d21db82da47c7684e5566db4aa81974250ee143f
Your Name <you@example.com>
2025-03-12 20:39:58 -0700

    removed unnecessary lines

--- frontend/src/pages/Study.jsx
+++ frontend/src/pages/Study.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { fetchFlashcardsByTopic } from '../components/ApiCall';
 import Flashcard from '../components/Flashcard/Flashcard';
+import './Study.css'
 
 const keys = {
   up: 'ArrowUp',
@@ -10,6 +11,12 @@ const keys = {
   right: 'ArrowRight'
 }
 
+const accuracyColorClasses = [
+  { range: [0, 25], colorClass: 'low-acc'},
+  { range: [25, 75], colorClass: 'medium-acc'},
+  { range: [75, 100], colorClass: 'high-acc' }
+]
+
 function Study() {
   const navigate = useNavigate();
   const { topic } = useParams();
@@ -17,8 +24,9 @@ function Study() {
   const [mode, setMode] = useState('manual')
   const [flashcards, setFlashcards] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0)
+  const [cardsMarked, setCardsMarked] = useState(0)
   const [numCorrect, setNumCorrect] = useState(0)
-  const [accuracy, setAccuracy] = useState(0)
+  const [accuracy, setAccuracy] = useState(null)
 
   useEffect(() => { 
     const fetchFlashcards = async () => {
@@ -42,25 +50,26 @@ function Study() {
     }
   }, [darkMode]);
 
+  useEffect(() => {
+    if (cardsMarked > 0) {
+      setAccuracy((numCorrect / cardsMarked) * 100)
+      }
+  }, [numCorrect, cardsMarked])
+
   const handleKeyPress = (event) => {
     // Prevents screen from moving up/down on up/down arrow keys 
     if (event.key === keys.down || event.key === keys.up)  { event.preventDefault() }
 
     setCurrentIndex((prevIndex) => {
-      if (event.key === keys.right) {
-        if (prevIndex + 1 < flashcards.length) return prevIndex + 1
-        else return prevIndex
+      if (event.key === keys.right && prevIndex + 1 < flashcards.length) {
+        return prevIndex + 1
       }
-      else if (event.key === keys.left) {
-        if (prevIndex - 1 >= 0) return prevIndex - 1
-        else return prevIndex
+      else if (event.key === keys.left && prevIndex - 1 >= 0) {
+        return prevIndex - 1
       }
-      else {
-        return prevIndex
-      }
+      return prevIndex
     })
 
-    // Updating numCorrect also updates accuracy 
     setNumCorrect((prevNumCorrect) => {
       let newNumCorrect = prevNumCorrect
       if (event.key === keys.up && prevNumCorrect < flashcards.length) {
@@ -69,10 +78,26 @@ function Study() {
       else if (event.key === keys.down && prevNumCorrect > 0) {
         newNumCorrect = prevNumCorrect - 1
       }
-      const newAccuracy = (newNumCorrect / flashcards.length) * 100;
-      setAccuracy(newAccuracy);
-      return newNumCorrect;
+      return newNumCorrect
     })
+
+    let newCardsMarked = 0
+    setCardsMarked((prevCardsMarked) => {
+      if ((event.key === keys.up || event.key === keys.down) && prevCardsMarked < flashcards.length) {
+        newCardsMarked = prevCardsMarked + 1
+        return newCardsMarked
+      }
+      return prevCardsMarked
+    })
+  };
+
+  const accuracyColor = () => {
+    for (const { range, colorClass } of accuracyColorClasses) {
+      if (accuracy >= range[0] && accuracy <= range[1]) {
+        return colorClass;
+      }
+    }
+    return ''; // Default class if accuracy is out of range
   };
 
   useEffect(() => {
@@ -94,7 +119,8 @@ function Study() {
           </div>
           <div style={{ visibility: mode === 'manual' ? 'visible' : 'hidden' }}>
             <p>Press up key to mark cards correct, down key to mark incorrect</p>
-            <h3>Accuracy:  <span className='accuracy-span'> {Number(accuracy).toFixed(0)}% </span></h3>
+            <h3>Accuracy:  
+              <span className={`accuracy-span ${(cardsMarked === 0) ? 'black' : accuracyColor()}`}> {(cardsMarked === 0) ? '---' : `${Number(accuracy).toFixed(0)}%`} </span></h3>
           </div>
           <div className="flashcards-container">
             {flashcards.length > 0 && (
